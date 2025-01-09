76a72e91eff5e09943dbb13d3bf2a48347dee61e
Lospub <zhjyy002@gmail.com>
2025-01-09 11:38:53 -0700

    create footer component

---
+++ wisefolio-client/src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import WiseFolioLogo from '@/assets/icons/WiseFolio.svg?react';
+import TwitterIcon from '@/assets/icons/twitter.svg?react';
+import FacebookIcon from '@/assets/icons/facebook.svg?react';
+import LinkedInIcon from '@/assets/icons/linkedin.svg?react';
+import EmailIcon from '@/assets/icons/email.svg?react';
+import PhoneIcon from '@/assets/icons/phone.svg?react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+
+        </footer>
+    );
+};
+
+export default Footer;
