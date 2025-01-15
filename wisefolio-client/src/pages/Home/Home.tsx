99711f1754fbbfb875621c82f77d97ddbd696a28
Lospub <zhjyy002@gmail.com>
2025-01-15 01:56:16 -0700

    add links

--- wisefolio-client/src/pages/Home/Home.tsx
+++ wisefolio-client/src/pages/Home/Home.tsx
@@ -6,11 +6,24 @@ import SavingsIcon from "../../assets/icons/saving.svg?react";
 import AIIcon from "../../assets/icons/AI.svg?react";
 import SecurityIcon from "../../assets/icons/locker.svg?react";
 import RealTimeIcon from "../../assets/icons/time.svg?react";
+import { Link, useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleSignupClick = () => {
+        navigate("/register");
+    };
+
+    const handleLoginClick = () => {
+        navigate("/login");
+    };
+
     return (
         <>
             <div className="home">
+
+                {/* Home page hero section */}
                 <header className="home__hero">
                     <h1 className="home__hero-title">Simplify Your Finances with AI</h1>
                     <p className="home__hero-subtitle">Take control of your financial future with smart tracking and intelligent insights</p>
@@ -32,12 +45,16 @@ const Home = () => {
                             <p>AI Insights</p>
                         </div>
                     </article>
+
+                    {/* User Login or User register or Guest usage */}
                     <div className="home__buttons">
-                        <button className="home__signup-btn">Sign Up</button>
-                        <button className="home__login-btn">Login</button>
-                        <a href="#" className="home__guest-link">Continue as Guest</a>
+                        <button className="home__signup-btn" onClick={handleSignupClick}>Sign Up</button>
+                        <button className="home__login-btn" onClick={handleLoginClick}>Login</button>
+                        <Link to="/dashboard" className="home__guest-link">Continue as Guest</Link>
                     </div>
                 </header>
+
+                {/* Benefits */}
                 <section className="home__choose">
                     <h2 className="home__choose-title">Why Choose WiseFolio?</h2>
                     <div className="home__benefits">
