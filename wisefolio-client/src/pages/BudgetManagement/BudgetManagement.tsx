a4187143ff1fb3eb9972a1c0726d39769679ca4b
Lospub <zhjyy002@gmail.com>
2025-01-17 16:13:49 -0700

    implement header and footer

--- wisefolio-client/src/pages/BudgetManagement/BudgetManagement.tsx
+++ wisefolio-client/src/pages/BudgetManagement/BudgetManagement.tsx
@@ -1,9 +1,25 @@
-const BudgetManagement  = () => {
-    return (
-        <>
-            <h1>BudgetManagement </h1>
-        </>
-    );
+import { useState } from "react";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/SideBar/SideBar";
+
+const BudgetManagement = () => {
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+  const [duration, setDuration] = useState("");
+
+  return (
+    <div className="container">
+      <Header />
+      <div className="budget">
+        <Sidebar />
+        <section className="budget-section">
+
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
-export default BudgetManagement ;
+export default BudgetManagement;
