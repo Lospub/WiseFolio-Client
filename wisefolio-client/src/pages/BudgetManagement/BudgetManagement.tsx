8f4fbf403bed1a70c2a42666efe2b08223151ea6
Lospub <zhjyy002@gmail.com>
2025-01-17 17:33:45 -0700

    implement budget form

--- wisefolio-client/src/pages/BudgetManagement/BudgetManagement.tsx
+++ wisefolio-client/src/pages/BudgetManagement/BudgetManagement.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/SideBar/SideBar";
+import DropDownIcon from "../../assets/icons/dropdown.svg?react"
+import "./BudgeManagement.scss"
 
 const BudgetManagement = () => {
   const [category, setCategory] = useState("");
@@ -13,11 +14,59 @@ const BudgetManagement = () => {
       <Header />
       <div className="budget">
         <Sidebar />
-        <section className="budget-section">
+        <section className="budget__section">
+          <form className="budget__form-details">
+            <h2 className="budget__form-title">Create New Budget</h2>
+            <div className="budget__dropdown-container">
+              <select
+                id="category"
+                className="budget__dropdown"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              >
+                <option value="" disabled hidden>Please select category</option>
+                <option value="food">Food</option>
+                <option value="transport">Transport</option>
+                <option value="entertainment">Entertainment</option>
+                <option value="others">Others</option>
+              </select>
+              <DropDownIcon className="budget__dropdown-icon" />
+            </div>
 
+            <div className="form-group">
+              <input
+                type="number"
+                id="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter amount"
+              />
+            </div>
+
+            <div className="budget__dropdown-container">
+              <select
+                id="duration"
+                className="budget__dropdown"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+              >
+                <option value="" disabled hidden>
+                  Please select duration
+                </option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+                <option value="yearly">Yearly</option>
+              </select>
+              <DropDownIcon className="budget__dropdown-icon" />
+            </div>
+
+            <button type="submit" className="budget__button">
+              Create Budget
+            </button>
+          </form>
         </section>
       </div>
-      <Footer />
     </div>
   );
 };
