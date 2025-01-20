12b9af26ca2de11de8091995f86f455770476bbe
Lospub <zhjyy002@gmail.com>
2025-01-19 23:35:41 -0700

    add date picker and form inputs with form to SavingsGoals page

--- wisefolio-client/src/pages/SavingsGoals/SavingsGoals.tsx
+++ wisefolio-client/src/pages/SavingsGoals/SavingsGoals.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/SideBar/SideBar";
 import "./SavingsGoals.scss";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import CalenderIcon from "../../assets/icons/calendar.svg?react"
 
 const SavingsGoals = () => {
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  
   return (
     <div className="container">
       <Header />
@@ -11,6 +17,34 @@ const SavingsGoals = () => {
         <section className="savings__section">
           <form className="savings__form-details">
             <h2 className="savings__form-title">Create New Savings Goal</h2>
+            <input type="text" placeholder="Goal Name" className="savings__input" />
+            <input type="number" placeholder="Goal Amount" className="savings__input" />
+            <div className="savings__date">
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date: Date | null) => {
+                  if (date) {
+                    setSelectedDate(date);
+                  }
+                }}
+                dateFormat="MM/dd/yyyy"
+                customInput={
+                  <div className="savings__custom">
+                    <input
+                      className="savings__date-input"
+                      type="text"
+                      value={
+                        selectedDate
+                          ? selectedDate.toLocaleDateString("en-US")
+                          : ""
+                      }
+                      readOnly
+                    />
+                    <CalenderIcon className="savings__date-icon" />
+                  </div>
+                }
+              />
+            </div>
             <button className="savings__button">Create</button>
           </form>
         </section>
